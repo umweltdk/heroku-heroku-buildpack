96e86aceac32b8deaeae81d0b9c12e3521a206e0
Emil Bay <hello@tixz.dk>
2014-04-01 12:48:59 +0200

    Moved utils to common.sh

---
+++ bin/common.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+status() {
+  echo "-----> $*"
+}
+
+error() {
+  echo " !     $*" >&2
+  exit 1
+}
+
+indent() {
+  c='s/^/       /'
+  case $(uname) in
+    Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+    *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+  esac
+}
