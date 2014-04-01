b05979024fec7f437c37f5cbd20d2a1fe58c5fbc
Emil Bay <hello@tixz.dk>
2014-04-01 15:21:01 +0200

    Use local bundler

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -5,9 +5,11 @@
 	export LANG=en_US.UTF-8
 
 	status "Installing bundler"
-	/usr/bin/env gem install bundler
+	/usr/bin/env gem install -i $cache_dir/gems/ bundler
+
+	bundler_dir=$cache_dir/gems/bin
 
 	status "Installing gems"
-	bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin --deployment | indent
+	$bundler_dir/bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin --deployment | indent
 )
 export PATH=$PATH:vendor/bundle/bin
