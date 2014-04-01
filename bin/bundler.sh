28dfdbf460549e99754cd5064e93cf3d219b0e11
Emil Bay <hello@tixz.dk>
2014-04-01 15:16:51 +0200

    Just trying something silly

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -4,6 +4,9 @@
 	export LC_ALL=en_US.UTF-8
 	export LANG=en_US.UTF-8
 
+	status "Installing bundler"
+	/usr/bin/env gem install bundler
+
 	status "Installing gems"
 	bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin --deployment | indent
 )
