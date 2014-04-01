d8fdce33573b6a4fded880c59584c523cbe25dc9
Emil Bay <hello@tixz.dk>
2014-04-01 15:42:43 +0200

    Fixed paths once again

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -5,12 +5,12 @@
 	export LANG=en_US.UTF-8
 
 	export GEM_HOME=$cache_dir/local-gems
-	export GEM_PATH=$GEM_PATH/gems
+	export GEM_PATH=$GEM_HOME/gems
 
 	status "Installing bundler"
 	/usr/bin/env gem install bundler
 
 	status "Installing gems"
-	$GEM_PATH/bin/bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin --deployment | indent
+	$GEM_HOME/bin/bundle install --without development:test --path vendor/bundle --binstubs vendor/bundle/bin --deployment | indent
 )
 export PATH=$PATH:vendor/bundle/bin
