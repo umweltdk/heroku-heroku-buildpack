581a4a116763b5ebcae89f3c4227523f4719cfc4
Emil Bay <hello@tixz.dk>
2014-04-15 17:59:47 +0200

    Updated paths

--- bin/bundler.sh
+++ bin/bundler.sh
@@ -1,56 +1,59 @@
 #!/usr/bin/env bash
 (
-	GEM_CACHE=$cache_dir/.gem/ruby/1.9.1
-	BUNDLE_CACHE=$cache_dir/vendor/bundle
+    GEM_BUILD=$build_dir/.gem
+    GEM_CACHE=$cache_dir/.gem
 
-	if [ -d "$env_dir" ]; then
-	  status "Exporting config vars to environment"
-	  export_env_dir $env_dir
-	fi
+    BUNDLE_BUILD=$build_dir/vendor/bundle
+    BUNDLE_CACHE=$cache_dir/vendor/bundle
 
-	#Fetch cache
-	if test -d $GEM_CACHE; then
-		status "Restoring gems from cache"
-		mkdir -p $GEM_HOME
-		cp -r $GEM_CACHE $GEM_HOME
-	fi
+    if [ -d "$env_dir" ]; then
+      status "Exporting config vars to environment"
+      export_env_dir $env_dir
+    fi
 
-
-	status "Installing bundler"
-	#gem install bundler |indent
-	gem install bundler | indent
-
-	#Purge cache
-	rm -rf $cache_dir/.gem/ruby/1.9.1
-
-	#Rebuild cache
-	if test -d $GEM_HOME; then
-		status "Rebuilding gem cache"
-		mkdir -p $GEM_CACHE
-		cp -r $GEM_HOME $GEM_CACHE
-	fi
+    #Fetch cache
+    if test -d $GEM_CACHE; then
+        status "Restoring gems from cache"
+        mkdir -p $GEM_BUILD
+        cp -R $GEM_CACHE $GEM_BUILD
+    fi
 
 
-	if test -d $BUNDLE_CACHE; then
-		status "Restoring bundle directory from cache"
-		mkdir -p $BUNDLE_HOME
-		cp -r $BUNDLE_CACHE $BUNDLE_HOME
-	fi
+    status "Installing bundler"
+    #gem install bundler |indent
+    gem install bundler | indent
 
-	status "Installing gems"
-	bundle install --gemfile=$build_dir/Gemfile \
-				   --without development:test \
-				   --path $BUNDLE_HOME \
-				   --binstubs $BUNDLE_HOME/bin \
-				   --deployment \
-		| indent
+    #Purge cache
+    rm -rf $GEM_CACHE
 
-	#Purge cache
-	rm -rf $BUNDLE_CACHE
+    #Rebuild cache
+    if test -d $GEM_BUILD; then
+        status "Rebuilding gem cache"
+        mkdir -p $GEM_CACHE
+        cp -R $GEM_BUILD $GEM_CACHE
+    fi
 
-	if test -d $BUNDLE_HOME; then
-		status "Rebuilding bundle directory cache"
-		mkdir -p $BUNDLE_CACHE
-		cp -r $BUNDLE_HOME $BUNDLE_CACHE
-	fi
+
+    if test -d $BUNDLE_CACHE; then
+        status "Restoring bundle directory from cache"
+        mkdir -p $BUNDLE_BUILD
+        cp -R $BUNDLE_CACHE $BUNDLE_BUILD
+    fi
+
+    status "Installing gems"
+    bundle install --gemfile=$build_dir/Gemfile \
+                   --without development:test \
+                   --path $BUNDLE_BUILD \
+                   --binstubs $BUNDLE_BUILD/bin \
+                   --deployment \
+        | indent
+
+    #Purge cache
+    rm -rf $BUNDLE_CACHE
+
+    if test -d $BUNDLE_BUILD; then
+        status "Rebuilding bundle directory cache"
+        mkdir -p $BUNDLE_CACHE
+        cp -R $BUNDLE_BUILD $BUNDLE_CACHE
+    fi
 )
